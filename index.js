a96048f3da2ac055dd5cfd25ed59cf9033cae574
Bilal Ghauri <66202403+Bilal-Ghauri@users.noreply.github.com>
2023-05-28 00:02:16 +0500

    added Access-Control-Allow-Origin
    added Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -15,12 +15,19 @@ const corsOptions = {
     optionSuccessStatus: 200
 }
 
-// app.use(function (req, res, next) {
-//     req.connection.setNoDelay(true)
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://instagram-frontend-omega.vercel.app/');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
