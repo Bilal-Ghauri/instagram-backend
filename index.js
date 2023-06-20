4df7c9d13445bae6b9d0205feed91ab9da18018a
Bilal Ghauri <66202403+Bilal-Ghauri@users.noreply.github.com>
2023-06-20 23:59:52 +0500

    added cors
    added cors

--- index.js
+++ index.js
@@ -22,14 +22,29 @@ app.use(cors({ origin: "*" }));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
 
 app.get('/', async (req, res) => {
     res.send('hello')
 })
 
-app.use('/user', userRoute)
-app.use('/post', postRoute)
+app.use('/user', allowCors(userRoute))
+app.use('/post', allowCors(postRoute))
 
 const startServer = async () => {
     try {
